93f743aa7f00e0651bd44e4275609699f2461d95
Andrei Vasile <andreibvasile@gmail.com>
2024-01-22 01:03:20 +0000

    changed eyeliner to only show jumps when f/F is pressed

--- lua/andyterrify/lazylazy/init.lua
+++ lua/andyterrify/lazylazy/init.lua
@@ -20,11 +20,17 @@ return {
   },
   "mbbill/undotree",
 
-  {
-    "tpope/vim-fugitive",
-  },
+  "tpope/vim-fugitive",
 
-  'jinh0/eyeliner.nvim',
+  {
+    'jinh0/eyeliner.nvim',
+    config = function()
+      require 'eyeliner'.setup {
+        highlight_on_key = true, -- show highlights only after keypress
+        dim = true            -- dim all other characters if set to true (recommended!)
+      }
+    end
+  },
 
   {
     -- Set lualine as statusline
