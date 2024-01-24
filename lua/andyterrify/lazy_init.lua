9a2a7536cf8e7defa9241208d4176b0bb36d7366
Andrei Vasile <andreibvasile@gmail.com>
2024-01-24 12:17:58 +0000

    modified lazy setup to disable unnecessary nvim plugins

--- lua/andyterrify/lazy_init.lua
+++ lua/andyterrify/lazy_init.lua
@@ -1,14 +1,28 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("andyterrify.lazylazy")
+require("lazy").setup(
+    { spec = { { import = "andyterrify.lazylazy" } }
+    },
+    {
+        checker = { enabled = false },
+        performance = {
+            rtp = {
+                "gzip",
+                "tarPlugin",
+                "tohtml",
+                "tutor",
+                "zipPligin"
+            }
+        }
+    })
